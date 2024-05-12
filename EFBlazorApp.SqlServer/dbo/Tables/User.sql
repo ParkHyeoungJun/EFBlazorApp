41e285f4b7f2c813e1e91a0dc7d1a5408667eb8d
박형준 <phj830914@gmail.com>
2024-05-12 21:13:21 +0900

    Sql 쿼리 관리 위한 프로젝트 생성

---
+++ EFBlazorApp.SqlServer/dbo/Tables/User.sql
@@ -0,0 +1,23 @@
+﻿--DECLARE @pwd VARBINARY(MAX);
+--SET @pwd = PWDENCRYPT('123');
+ 
+--SELECT PWDCOMPARE('123', @pwd)
+--SELECT PWDCOMPARE('12345', @pwd)
+
+
+CREATE TABLE [dbo].[User]
+(
+	 -- Idx  Bigint Identity(1,1)         , 
+      UserId Nvarchar(20)   Primary key ,
+      [Password]  varbinary(128)    Not Null    ,
+      FirstName Varchar(10)         Not Null    ,
+      LastName  Varchar(20)         Not Null    ,
+      Email     Varchar(255)                    ,
+      PhoneNumber Varchar(15)                   ,
+      DateOfBirth DateTime Default(GetDate())   ,
+      Gender Char(1)       Default('M')         ,
+      RegistrationDate datetime Default(Getdate()),
+      LastLoginDate  Smalldatetime Default(null),
+      LoginFailed tinyint Default(0)            ,
+      DelYn Char(1) Default('N')
+)
